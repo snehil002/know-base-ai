f22ca17fcc97b63ffd75416b57226c5c03c0c903
Snehil Kumar <88778723+snehil002@users.noreply.github.com>
2026-05-15 21:21:27 +0530

    refactor: modified example secret file location
    add example secret files to backend/secrets
    add new env var for GCS bucket name
    remove unnecessary env var

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,10 +2,10 @@ module.exports = {
   PORT: process.env.PORT || 5000,
   MONGODB_URI: process.env.MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
-  NODE_ENV: process.env.NODE_ENV,
   FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   PINECONE_API_KEY: process.env.PINECONE_API_KEY,
   PINECONE_INDEX_NAME: process.env.PINECONE_INDEX_NAME,
   PINECONE_INDEX_HOST: process.env.PINECONE_INDEX_HOST,
+  GCS_BUCKET_NAME: process.env.GCS_BUCKET_NAME,
 };
