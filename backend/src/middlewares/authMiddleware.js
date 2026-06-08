545d372d6884791aa750785fadb5178cf18e24cf
Snehil Kumar <88778723+snehil002@users.noreply.github.com>
2026-06-08 21:40:45 +0530

    fix: add error handling to auth middleware

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,40 +1,61 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config/env");
-const User = require("../features/auth/auth.model");
+const authModel = require("../features/auth/auth.model");
 
 const authMiddleware = async (req, res, next) => {
   try {
     if (!req.cookies || !req.cookies["auth-token"]) { // no cookie/expired
-      const err = new Error("You are not authenticated. Please login.");
-      err.statusCode = 401;
+      const err = new Error("No cookie or expired cookie.");
+      err.forFrontend = {
+        message: "You are not authenticated. Please login.",
+        statusCode: 401,
+      };
       throw err;
     }
 
     const authToken = req.cookies["auth-token"]; // JWT token extracted from req cookie
 
-    const { userId } = jwt.verify(authToken, JWT_SECRET);
+    let userId;
+    try {
+      ({ userId } = jwt.verify(authToken, JWT_SECRET));
+    } catch (err) {
+      err.forFrontend = {
+        message: "You are not authenticated. Please login.",
+        details: { deAuthenticateUser: true },
+        statusCode: 401,
+      };
+      throw err;
+    }
 
     // DB Read query
     // returns the user document (without password: see model file) if found, otherwise null
-    const existing = await User.findById(userId);
+    const existing = await authModel.User.findById(userId);
 
     if (!existing) {
-      const err = new Error("Something went wrong. You have been logged out.");
-      err.statusCode = 401;
+      const err = new Error("User does not exist.");
+      err.forFrontend = {
+        message: "Something went wrong. You have been logged out.",
+        details: { deAuthenticateUser: true },
+        statusCode: 401,
+      };
       throw err;
     }
 
     req.user = {
       _id: existing._id,
       fullName: existing.fullName,
-      companyName: existing.companyName,
       companyEmail: existing.companyEmail,
-      role: existing.role,
     };
 
     next();
-  } catch(err) {
-    err.details = { deAuthenticateUser: true };
+  } catch (err) {
+    if (!err.forFrontend) {
+      err.forFrontend = {
+        message: "Something went wrong. You have been logged out.",
+        details: { deAuthenticateUser: true },
+        statusCode: 500,
+      };
+    }
     next(err);
   }
 };
