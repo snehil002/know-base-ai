1fab58352ad82e7624bd7771aa3ecd1aa2d5dcf6
Snehil Kumar <88778723+snehil002@users.noreply.github.com>
2026-06-19 18:57:08 +0530

    feat: create mongodb 'File' doc and send fileId and signedUrl when generating upload url

--- backend/src/features/files/files.controller.js
+++ backend/src/features/files/files.controller.js
@@ -3,11 +3,14 @@ const response = require("../../utils/response");
 
 exports.getUploadSignedUrl = async (req, res, next) => {
   try {
-    const { fileName } = req.body;
+    let uploaderId, workspaceId;
+    const { fileName, fileSizeInBytes, title, description } = req.body;
 
-    const url = await filesService.getUploadSignedUrl(fileName);
+    const { fileId, gcsFileName } = await filesService.createFile({ fileName, fileSizeInBytes, title, description, uploaderId, workspaceId });
 
-    response.sendSuccessResponse(res, "Successfully generated upload signed URL", { url });
+    const uploadSignedUrl = await filesService.getUploadSignedUrl(gcsFileName);
+
+    response.sendSuccessResponse(res, "Successfully generated upload signed URL", { fileId, uploadSignedUrl });
 
   } catch (err) {
     next(err);
--- backend/src/features/files/files.service.js
+++ backend/src/features/files/files.service.js
@@ -1,6 +1,44 @@
 const bucket = require("../../config/gcsBucket");
+const filesModel = require("./files.model");
 
-exports.getUploadSignedUrl = async (fileName) => {
+exports.createFile = async ({
+  fileName,
+  fileSizeInBytes,
+  title,
+  description,
+  uploaderId,
+  workspaceId,
+}) => {
+  try {
+    const file = new filesModel.File({
+      fileName,
+      fileSizeInBytes,
+      title,
+      description,
+      uploaderId,
+      workspaceId
+    });
+    
+    const gcsFileName = `${file._id}_${fileName}`;
+    file.gcsFileName = gcsFileName;
+    
+    await file.save();
+    
+    return {
+      fileId: file._id,
+      gcsFileName
+    };
+
+  } catch (err) {
+    err.forFrontend = {
+      message: "Something went wrong. Please try again",
+      statusCode: 500,
+    };
+    throw err;
+  }
+};
+
+exports.getUploadSignedUrl = async (gcsFileName) => {
   try {
     const writeOptions = {
       version: 'v4',
@@ -9,7 +47,7 @@ exports.getUploadSignedUrl = async (fileName) => {
       contentType: 'application/pdf',
     };
     
-    const [url] = await bucket.file(fileName).getSignedUrl(writeOptions);
+    const [url] = await bucket.file(gcsFileName).getSignedUrl(writeOptions);
     return url;
 
   } catch (err) {
